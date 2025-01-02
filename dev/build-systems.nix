5ef589cf1799d0d63f76d789ef748834faab37d9
adisbladis <adisbladis@gmail.com>
2025-01-02 17:44:08 +1300

    checks: Remove dependency on hatch-requirements-txt from pyproject.nix base set

--- dev/build-systems.nix
+++ dev/build-systems.nix
@@ -556,6 +556,35 @@ builtins.mapAttrs (_n: v: final.callPackage v { }) {
         };
     });
 
+  hatch-requirements-txt =
+    {
+      stdenv,
+      python3Packages,
+      pyprojectHook,
+      resolveBuildSystem,
+    }:
+    stdenv.mkDerivation (_finalAttrs: {
+      inherit (python3Packages.hatch-requirements-txt)
+        pname
+        version
+        src
+        meta
+        ;
+
+      passthru.dependencies = {
+        hatchling = [ ];
+        packaging = [ ];
+      };
+
+      nativeBuildInputs =
+        [
+          pyprojectHook
+        ]
+        ++ resolveBuildSystem {
+          hatchling = [ ];
+        };
+    });
+
   pathspec =
     {
       stdenv,
